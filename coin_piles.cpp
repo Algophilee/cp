721f690661c140ad8910e956922010c851a3de49
algophilee <annoy>
2026-05-27 20:31:21 +0530

    pushing coin flip

--- coin_piles.cpp
+++ coin_piles.cpp
@@ -2,16 +2,28 @@
 
 using namespace std;
 
+void solve() {
+    long long a, b;
+    cin >> a >> b;
+    
+    
+    if (a > b) swap(a, b);
+    
+    if ((a + b) % 3 == 0 && a * 2 >= b) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   long n;
-  cin >> n;
+  cin >> n ;
 
-  while(n--){ 
-  long long a , b;
-  cin >> a >> b;
-  
+  while(n--){
+    solve();
   }
   return 0;
 }
